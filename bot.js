93c18ef99779093b431b0ab93b4e3e2ffe2bf47b
crewgamer <32820583+crewgamer@users.noreply.github.com>
2017-10-24 22:00:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 const Discord = require("discord.js")
 const client = new Discord.Client();
 const prefix = ("!")
-var args = (proccess.env.ARGUMENTS)
-var wordstoecho = (process.env.WORDSTOECHO)
-var game = (proccess.env.GAMEARGS)
 client.login(client.login(process.env.BOT_TOKEN))
 client.on("ready", function() {
   console.log("Ready as: Testingbot3k")
@@ -16,8 +13,8 @@ client.on("message", function(msg) {
       return;
     }
     msg.channel.send("Your Message was set :white_check_mark: " + msg.author )
-let args = msg.content.split(` `).slice(1);
-let game = args.join(` `)
+let ARGUMENTS = msg.content.split(` `).slice(1);
+let GAMEARGS = args.join(` `)
 console.log(game)
 client.user.setGame(game)
 }
@@ -29,8 +26,8 @@ client.on("message", function(msg) {
       msg.channel.send(":x: Bot Owner Only Sorry Bot owners only " +  msg.author)
       return;
     }
-let args = msg.content.split(` `).slice(1);
-let wordstoecho = args.join(` `)
+let ARGUMENTS = msg.content.split(` `).slice(1);
+let WORDSTOECHO = args.join(` `)
 console.log(`${msg.author.tag} made the bot say ` + wordstoecho)
 msg.channel.send(wordstoecho)
 }
@@ -41,19 +38,10 @@ client.on("message", function(msg) {
 if(msg.content.startsWith (prefix + "sendto")) {
 //msg.guild.member(msg.mentions.members.first()).removeRole('371698466436677642').catch(error => console.log(error));
 msg.channel.send("Dmed " + msg.mentions.members.first() +  " :white_check_mark:  "  + msg.author)
-let args = msg.content.split(` `).slice(1);
-let Messageinners = args.join(` `)
+let ARGUMENTS = msg.content.split(` `).slice(1);
+let MESSAGEINNERS = args.join(` `)
 msg.mentions.members.first().sendMessage(Messageinners)
 console.log(`${msg.author.tag} sent a message to ` + msg.mentions.members.first()  + " saying   " + Messageinners)
 //client.channels.get('371699391431704587').sendMessage('User Unwhitelisted by ' + msg.author)
 }
-})
-
-client.on("message", function(msg) {
- if(msg.content === prefix + "ping") {
-    msg.channel.send(`Pinging...`).then(function(m){
-      m.edit(`Pong! took ${m.createdTimestamp - msg.createdTimestamp} ms`)
-    })23
-   }
-
 })
